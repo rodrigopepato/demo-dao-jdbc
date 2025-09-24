5147cae6ce3095d59591f4f13babbc7e60bbd40f
rodrigopepato <rodrigo.pepato@gmail.com>
2025-09-24 18:38:21 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,10 +33,20 @@ public class Program {
         }
         System.out.println();
 
-        System.out.println("=== TEST 4: seller insert ===");
+        /*System.out.println("=== TEST 4: seller insert ===");
         Seller newSeller = new Seller(null, "Maria", "maria@gmail.com", LocalDate.of(1980,4,16), 4500.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id: " + newSeller.getId());
+        System.out.println();*/
+
+        System.out.println("=== TEST 5: seller update ===");
+        seller = sellerDao.findById(14);
+        seller.setName("Pedro Guilherme");
+        seller.setEmail("pedro@gmail.com");
+        seller.setBirthDate(LocalDate.of(1998, 04, 26));
+        seller.setSalary(1000.00);
+        sellerDao.update(seller);
+        System.out.println("Update completed!");
 
     }
 }
